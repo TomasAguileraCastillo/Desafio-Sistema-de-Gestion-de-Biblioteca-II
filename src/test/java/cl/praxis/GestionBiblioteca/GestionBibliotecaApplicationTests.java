2b4ca64818359c8700bd41fe57e5f9fc8d7b2567
Tomás Aguilera <136927556+TomasAguileraCastillo@users.noreply.github.com>
2024-08-29 18:05:00 -0400

    Actualizacion de codigo

---
+++ src/test/java/cl/praxis/GestionBiblioteca/GestionBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.praxis.GestionBiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
